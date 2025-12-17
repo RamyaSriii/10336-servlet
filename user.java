f1c89d811c9502a52da93c002223ad6624485be5
RamyaSriii <118435552+RamyaSriii@users.noreply.github.com>
2025-12-17 21:07:44 +0530

    user.java

---
+++ user.java
@@ -0,0 +1,51 @@
+package com.sat.tmf.tktbooking.model;
+
+public class User {
+	int id;
+	String firstName;
+	String lastName;
+	String email;
+	String phNo;
+	
+	
+	public User(int id, String firstName, String lastName, String email, String phNo) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phNo = phNo;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhNo() {
+		return phNo;
+	}
+	public void setPhNo(String phNo) {
+		this.phNo = phNo;
+	}
+	
+}
